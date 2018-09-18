b8e244bf616f1fa77da9c53f045cb6dc5559077c
MattiasL90 <mattias.liljegren@hotmail.se>
2018-09-18 12:49:14 +0200

    FIXAT

--- Yatzy183333/Yatzy183333/MainWindow.xaml.cs
+++ Yatzy183333/Yatzy183333/MainWindow.xaml.cs
@@ -35,23 +35,126 @@ namespace Yatzy183333
             dices = g.rollDices(savedDice, dices);
             setLabels();
             round++;
-            if (round == 2)
+            if (round == 3)
             {
                 round = 0;
                 setDices();
+                btnRoll.IsEnabled = false;
+                btnSave.IsEnabled = true;
             }
             updateDg();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int type = checkSave();
+            Game g = new Game();
+            int score = checkScore();
+            g.saveScore(score, type, id);
+        }
+
+        public int checkScore()
+        {
+            int score = 0;
+            if (cbOne.IsChecked == true)
+            {
+                int s = dices[0];
+                score = score + s;
+            }
+            if (cbTwo.IsChecked == true)
+            {
+                int s = dices[1];
+                score = score + s;
+            }
+            if (cbThree.IsChecked == true)
+            {
+                int s = dices[2];
+                score = score + s;
+            }
+            if (cbFour.IsChecked == true)
+            {
+                int s = dices[3];
+                score = score + s;
+            }
+            if (cbFive.IsChecked == true)
+            {
+                int s = dices[4];
+                score = score + s;
+            }
+            return score;
+        }
+
+        public int checkSave()
+        {
+            int saved = 0;
+            if (rbOnes.IsChecked == true)
+            {
+                saved = 1;
+            }
+            else if (rbTwos.IsChecked == true)
+            {
+                saved = 2;
+            }
+            else if (rbThrees.IsChecked == true)
+            {
+                saved = 3;
+            }
+            else if (rbFours.IsChecked == true)
+            {
+                saved = 4;
+            }
+            else if (rbFives.IsChecked == true)
+            {
+                saved = 5;
+            }
+            else if (rbSixes.IsChecked == true)
+            {
+                saved = 6;
+            }
+            else if (rbPair.IsChecked == true)
+            {
+                saved = 7;
+            }
+            else if (rbTwopair.IsChecked == true)
+            {
+                saved = 8;
+            }
+            else if (rbTriads.IsChecked == true)
+            {
+                saved = 9;
+            }
+            else if (rbQuads.IsChecked == true)
+            {
+                saved = 10;
+            }
+            else if (rbHouse.IsChecked == true)
+            {
+                saved = 11;
+            }
+            else if (rbLadderl.IsChecked == true)
+            {
+                saved = 12;
+            }
+            else if (rbLadderb.IsChecked == true)
+            {
+                saved = 13;
+            }
+            else if (rbChance.IsChecked == true)
+            {
+                saved = 14;
+            }
+            else if (rbYatzy.IsChecked == true)
+            {
+                saved = 15;
+            }
+            return saved;
+        }
+
         public void updateDg()
         {
             Player p = new Player();
             dgList.ItemsSource = null;
             dgList.ItemsSource = p.player;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
         public void setLabels()
