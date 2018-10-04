0ed8ad0f69be13369bc81efe3a3fd700658ea718
MattiasL90 <mattias.liljegren@hotmail.se>
2018-10-04 13:25:11 +0200

    Fixat makegame

--- Yatzy183333/Yatzy183333/Login.xaml.cs
+++ Yatzy183333/Yatzy183333/Login.xaml.cs
@@ -30,11 +30,26 @@ namespace Yatzy183333
             string nameOne = tbOne.Text;
             string nameTwo = tbTwo.Text;
             string nameThree = tbThree.Text;
+            SQL s = new SQL();
+            int type = GetStyle();
+            //if (rbTwoPlayers.IsChecked == true)
+            //{
+            //    bool one = s.CheckName(nameOne);
+            //    bool two = s.CheckName(nameTwo);
+            //}
+            //else if (rbThreePlayers.IsChecked == true)
+            //{
+            //    bool one = s.CheckName(nameOne);
+            //    bool two = s.CheckName(nameTwo);
+            //    bool three = s.CheckName(nameThree);
+            //}
+
             //Classic g = new Classic();
             //Forced f = new Forced();
             if (rbClassic.IsChecked == true)
             {
                 Classic g = new Classic();
+                CheckGame(nameOne, nameTwo, nameThree, type);
                 this.Hide();
                 g.addPlayer(nameOne, 1);
                 g.addPlayer(nameTwo, 2);
@@ -46,6 +61,7 @@ namespace Yatzy183333
             else if (rbForced.IsChecked == true)
             {
                 Forced g = new Forced();
+                CheckGame(nameOne, nameTwo, nameThree, type);
                 this.Hide();
                 g.addPlayer(nameOne, 1);
                 g.addPlayer(nameTwo, 2);
@@ -54,20 +70,47 @@ namespace Yatzy183333
                 w1.ShowDialog();
 
             }
-
-           
-            //g.addPlayer(nameOne, 1);
-            //g.addPlayer(nameTwo, 2);
-            //g.addPlayer(nameThree, 3);
-
-            
-            //MainWindow w1 = new MainWindow(nameOne, nameTwo, nameThree);
-            //MainWindow w1 = new MainWindow(g);
-            //w1.ShowDialog();
         }
 
-      
+        private void CheckGame(string nameOne, string nameTwo, string nameThree, int type)
+        {
+            SQL s = new SQL();
+            bool one = false;
+            bool two = false;
+            bool three = false;
+            if (cbThree.IsChecked == false)
+            {
+                one = s.CheckName(nameOne);
+                two = s.CheckName(nameTwo);
+                if (one == true && two == true)
+                {
+                    s.MakeGame(type);
+                }
+            }
+            else if (cbThree.IsChecked == true)
+            {
+                one = s.CheckName(nameOne);
+                two = s.CheckName(nameTwo);
+                three = s.CheckName(nameThree);
+                if (one == true && two == true && three == true)
+                {
+                    s.MakeGame(type);
+                }
+            }
+        }
 
-        
+        private int GetStyle()
+        {
+            int type = 0;
+            if (rbClassic.IsChecked == true)
+            {
+                type = 1;
+            }
+            else if  (rbForced.IsChecked == true)
+            {
+                type = 2;
+            }
+            return type;
+        }
     }
 }
--- Yatzy183333/Yatzy183333/SQL.cs
+++ Yatzy183333/Yatzy183333/SQL.cs
@@ -9,17 +9,69 @@ namespace Yatzy183333
 {
     class SQL
     {
-        public void addVisits(int vid, int eid)
+        public Boolean CheckName(string name)
         {
-            string stmt = "insert into visits (visitorid, employeeid) values (@vid, @eid)";
+            string stmt = "SELECT name FROM player WHERE name = @name";
+            Boolean yes = false;
 
             using (var conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString))
             {
                 conn.Open();
                 using (var cmd = new NpgsqlCommand(stmt, conn))
                 {
-                    cmd.Parameters.AddWithValue("@vid", vid);
-                    cmd.Parameters.AddWithValue("@eid", eid);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.ExecuteNonQuery();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string rname = reader.GetString(0);
+                            if (rname == name)
+                            {
+                                yes = true;
+                            }
+                        }
+                    }
+                }
+                conn.Close();
+            }
+            return yes;
+        }
+
+        public int GetId(string name)
+        {
+            string stmt = "SELECT player_id FROM player WHERE name = @name";
+            int id = 0;
+            using (var conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand(stmt, conn))
+                {
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.ExecuteNonQuery();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            id = reader.GetInt32(0);
+                        }
+                    }
+                }
+                conn.Close();
+            }
+            return id;
+        }
+
+
+        public void MakeGame(int type)
+        {
+            string stmt = "INSERT INTO game (gametype_id) VALUES (@type)";
+            using (var conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand(stmt, conn))
+                {
+                    cmd.Parameters.AddWithValue("@type", type);
                     cmd.ExecuteNonQuery();
                 }
                 conn.Close();
