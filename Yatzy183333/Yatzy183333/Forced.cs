df4a65060d969c5c484a28161cfb7b83458582b6
MrDaZe <43004443+MrDaZe@users.noreply.github.com>
2018-10-05 12:44:55 +0200

    fgdgdf

--- Yatzy183333/Yatzy183333/Forced.cs
+++ Yatzy183333/Yatzy183333/Forced.cs
@@ -408,7 +408,7 @@ namespace Yatzy183333
             bool fin = false;
             foreach (Player y in player)
             {
-                if (y.id == ppl && y.fsave == 1)
+                if (y.id == ppl && y.fsave == 15)
                 {
                     s.EndGame(matchid);
                     fin = true;
